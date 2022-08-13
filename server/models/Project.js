e7df6e582f0fe34223d05241f878d2c24498cd4c
muhsin7majeed <muhsin7ak@gmail.com>
2022-08-13 20:16:39 +0530

    FIX: Add Description Field to Project Model

--- server/models/Project.js
+++ server/models/Project.js
@@ -1,4 +1,4 @@
-const { Schema } = require("mongoose");
+const { Schema, model } = require("mongoose");
 
 const ProjectSchema = new Schema({
   name: {
@@ -11,6 +11,10 @@ const ProjectSchema = new Schema({
   clientId: {
     type: Schema.Types.ObjectId,
     ref: "Client",
+  },
+
+  description: {
+    type: String,
   },
 });
 
