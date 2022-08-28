013f7a4a6ea27a9a505c6cbd9d56d8c6f484200c
muhsin <muhsin7ak@gmail.com>
2022-08-28 16:44:11 +0530

    CHANGE: Mark name, status & client as required on Project Schema

--- server/models/Project.js
+++ server/models/Project.js
@@ -3,13 +3,16 @@ const { Schema, model } = require("mongoose");
 const ProjectSchema = new Schema({
   name: {
     type: String,
+    required: true,
   },
   status: {
     type: String,
+    required: true,
     enum: ["Pending", "In Progress", "Paused", "Completed"],
   },
-  clientId: {
+  client: {
     type: Schema.Types.ObjectId,
+    required: true,
     ref: "Client",
   },
 
