4dc0ec5c1ad2a6a0633b8da1be44974e2460436f
muhsin <muhsin7ak@gmail.com>
2022-08-28 16:43:25 +0530

    CHANGE: Mark name & email as required on Client Schema

--- server/models/Client.js
+++ server/models/Client.js
@@ -3,9 +3,11 @@ const { Schema, model, Buffer } = require("mongoose");
 const ClientSchema = new Schema({
   name: {
     type: String,
+    required: true,
   },
   email: {
     type: String,
+    required: true,
   },
   avatar: {
     type: String,
